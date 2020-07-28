a8343261f3a7417cfc197459ff2988f77747251a
kcsheng <kacysheng@mail.com>
2020-07-28 18:51:37 +1000

    initial commit

---
+++ lib/kcs_view_tool.rb
@@ -0,0 +1,7 @@
+require "kcs_view_tool/version"
+require "kcs_view_tool/renderer"
+
+module KcsViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kcs_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KcsViewTool
+  VERSION = "0.1.0"
+end
